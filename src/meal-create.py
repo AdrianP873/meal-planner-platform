4a3f8a40c07a8788475a85f95d2fa182143b95d5
AdrianP873 <adrian.m.plummer@gmail.com>
2020-10-26 14:20:52 +1100

    Added support for CORS with lambda proxy integration

--- src/meal-create.py
+++ src/meal-create.py
@@ -5,6 +5,7 @@ Add a meal to the database.
 import boto3
 import os
 import logging
+import json
 
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.INFO)
@@ -15,27 +16,33 @@ def handler(event, context):
     DYNAMODB_TABLE = os.getenv('MEAL_TABLE')
     meal_table = dynamodb.Table(DYNAMODB_TABLE)
 
-    try:
-        data = {
-            "meal": event["meal"],
-            "ingredients": event["ingredients"]
-        }
+    data = json.loads(event['body'])
+    
+    payload = {
+        "meal": data["meal"],
+        "ingredients": data["ingredients"]
+    }
 
-        meal_table.put_item(
-            Item=data
-        )
+    meal_table.put_item(
+        Item=data
+    )
 
-        logging.info("meal: {}, ingredients: {}".format(data["meal"], data["ingredients"]))
-        logging.info({"data": data})
+    logging.info("meal: {}, ingredients: {}".format(payload["meal"], payload["ingredients"]))
+    logging.info({"data": payload})
 
-        return_status = 200
-        return_body = {"message": "{} successfully added.".format(data["meal"])}
-    except KeyError as e:
-        logging.error(e)
-        return_status = 400
-        return_body = {"message": "missing data"}
+    return_status = 200
+    return_body = {"message": "{} successfully added.".format(payload["meal"])}
 
     return {
         "statusCode": return_status,
-        "body": return_body
+        'headers': {
+            'Access-Control-Allow-Headers': 'Content-Type',
+            'Access-Control-Allow-Origin': '*',
+            'Access-Control-Allow-Methods': 'OPTIONS,POST',
+            'Content-Type': 'application/json',
+        },
+        "body": json.dumps(return_body)
     }
+    
+    
+    
